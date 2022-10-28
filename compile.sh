275e2d13ea1edcb1bbb65bb166160f2e5a808285
Your Name <you@example.com>
2022-10-28 08:48:07 +0800

    Use a ( subshell ) to avoid having to cd back.

--- compile.sh
+++ compile.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 mkdir -p bc
-cd tests
-cfiles=$(ls .)
-for file in $cfiles; do
+(
+  cd tests
+  cfiles=$(ls .)
+  for file in $cfiles; do
     prefix=${file:0:6}
     bc_file="$prefix.bc"
     clang -emit-llvm -c -O0 -g3 $file -o $bc_file
     llvm-dis $bc_file
-done
-cd ..
+  done
+)
 mv tests/*.bc bc
 mv tests/*.ll bc
