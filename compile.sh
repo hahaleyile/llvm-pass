ad031259b86fe1efd9c2ce22fd557da6ebed5eb3
Your Name <you@example.com>
2022-10-29 22:38:51 +0800

    Improve get file basename method

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ mkdir -p bc
   cd tests
   cfiles=$(ls .)
   for file in $cfiles; do
-    prefix=${file:0:6}
+    prefix=$(basename -s .c "$file")
     bc_file="$prefix.bc"
     clang -emit-llvm -c -O0 -g3 $file -o $bc_file
     llvm-dis $bc_file
